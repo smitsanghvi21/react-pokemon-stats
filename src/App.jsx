44db06628b2283ea5bffc7a1711a3f4702be6253
smitsanghvi21 <sanghvismit02@gmail.com>
2022-08-17 10:26:35 +1200

    code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import myjson from "./myjson.json";
-import PokeMonData from "./PokeData";
-import SelectedItemStats from "./SelectedItemStats";
+import React, { useState } from 'react'
+import './App.css'
+import myjson from './myjson.json'
+import PokeMonData from './PokeMonData'
+import SelectedItemStats from './SelectedItemStats'
 
-function App() {
-  const [filter, setFilter] = useState("");
-  const [pokemon, pokemonSet] = useState([]);
-  const [selected, setSelected] = useState(null);
+function App () {
+  const [filter, setFilter] = useState('')
+  const [selected, setSelected] = useState(null)
 
- /* useEffect(() => {
-    const url = "http://localhost:3000/myjson.json";
-    fetch(url).then((res) => {
-      console.log(res);
-    });
-  }, []);*/
+  const onFilterChange = e => {
+    setFilter(e.target.value)
+  }
 
-  const onFilterChange = (e) => {
-    setFilter(e.target.value);
-  };
-
-  const onSelection = (pokemon) => {
-    setSelected(pokemon);
-  };
+  const onSelection = pokemon => {
+    setSelected(pokemon)
+  }
 
   return (
-    <div className="App">
-      <h1 className="title">Pokemon search</h1>
-      <input value={filter} onChange={onFilterChange} placeholder="search" />
-      {selected ? <SelectedItemStats {...selected} /> : ""}
-      <table class="table" width="100%">
+    <div className='App'>
+      <h1 className='title'>Pokemon search</h1>
+      <input value={filter} onChange={onFilterChange} placeholder='search' />
+      {selected ? <SelectedItemStats {...selected} /> : ''}
+      <table class='table' width='100%'>
         <thead>
           <tr>
             <th>Id</th>
@@ -40,9 +32,9 @@ function App() {
         </thead>
         <tbody>
           {myjson
-            .filter((item) => item.name.english.toLowerCase().includes(filter))
+            .filter(item => item.name.english.toLowerCase().includes(filter))
             .slice(0, 20)
-            .map((item) => (
+            .map(item => (
               <PokeMonData
                 pokemon={item}
                 pokeId={item.id}
@@ -52,7 +44,7 @@ function App() {
         </tbody>
       </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
